5ea829a9a08d11700fd37200fb8d8aad547fccf9
moosa <moosaabdullahi45@gmail.com>
2025-09-17 09:52:12 +0100

    fix build

--- frontend/kidashi_ui/src/states/api/endpoints/vendors/vendorApiSlice.ts
+++ frontend/kidashi_ui/src/states/api/endpoints/vendors/vendorApiSlice.ts
@@ -16,28 +16,28 @@ const vendorApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
         getVendorDetail: builder.mutation({
             query: ({ vendor_id }) => ({
-                url: '/vendor/get_vendor_details',
+                url: 'vendor/staff/get_vendor_details',
                 method: 'POST',
                 body: { id: vendor_id }
             }),
         }),
         fetchOnboardedVendors: builder.query<VendorResponse, Record<string, unknown> | void>({
             query: (filters = {}) => ({
-                url: '/vendor/fetch_onboarded_vendors',
+                url: 'vendor/staff/fetch_onboarded_vendors',
                 method: 'POST',
                 body: { ...filters }
             }),
         }),
         fetchPendingVendors: builder.query<VendorResponse, Record<string, unknown> | void>({
             query: (filters = {}) => ({
-                url: '/vendor/fetch_pending_vendors',
+                url: 'vendor/staff/fetch_pending_vendors',
                 method: 'POST',
                 body: { ...filters }
             }),
         }),
         updateVendorApplicationStatus: builder.mutation<{ status: boolean; message: string }, { vendor_id: string }>({
             query: ({ vendor_id }) => ({
-                url: '/vendor/update_vendor_application_status',
+                url: 'vendor/staff/update_vendor_application_status',
                 method: 'POST',
                 body: { id: vendor_id }
             }),
