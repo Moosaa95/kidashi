7b6bfd723b72c0dc7cca27d938b2225ea894679c
Basil Ilenikhena <ia7021x@gmail.com>
2025-09-24 15:50:37 +0100

    update: fix config DJANGO_SETTINGS_MODULE value

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kidashi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.kidashi")
 
 application = get_asgi_application()
--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kidashi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.kidashi")
 
 application = get_wsgi_application()
