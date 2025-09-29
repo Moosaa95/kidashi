0dce919a20a063fde19c812b9f20fd1b2d7c9d8a
moosa <moosaabdullahi45@gmail.com>
2025-09-29 18:30:56 +0100

    fix auth token

--- modules/security/mixins.py
+++ modules/security/mixins.py
@@ -1,15 +1,16 @@
-from rest_framework import exceptions
+from rest_framework import status
+from django.http import JsonResponse
 
 
 class IsPayrepAuthenticatedMixin:
     def dispatch(self, request, *args, **kwargs):
         auth_header = request.headers.get("Authorization", "")
         if not auth_header or not auth_header.startswith("Bearer "):
-            raise exceptions.AuthenticationFailed("PayRep authorization token required")
+            return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data=dict(status=False, message="PayRep authorization token required"))
 
         token = auth_header.replace("Bearer ", "").strip()
         if not token:
-            raise exceptions.AuthenticationFailed("Invalid PayRep token")
+            return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data=dict(status=False, message="Invalid PayRep token"))
 
         request.payrep_token = token
         return super().dispatch(request, *args, **kwargs)
